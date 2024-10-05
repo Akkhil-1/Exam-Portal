763c463e44b24c8ed1c4c20f6925bf325d646b8f
AKhil <akhilacharya343@gmail.com>
2024-10-05 22:34:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-background text-foreground text-8xl ">
+        hrllo
+        <Button>Hello Akhil</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
